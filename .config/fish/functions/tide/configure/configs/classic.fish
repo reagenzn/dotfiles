69496e24e6f490e4efa6294c8bea135217c24126
NatsukiFukazawa <aiooksa929@gmail.com>
2025-05-11 11:03:30 +0900

    feat:fish-ai追加

--- .config/fish/functions/tide/configure/configs/classic.fish
+++ .config/fish/functions/tide/configure/configs/classic.fish
@@ -38,7 +38,7 @@ tide_git_color_staged $_tide_color_gold
 tide_git_color_stash $_tide_color_green
 tide_git_color_untracked $_tide_color_light_blue
 tide_git_color_upstream $_tide_color_green
-tide_git_truncation_length 24
+tide_git_truncation_length 50
 tide_git_truncation_strategy
 tide_go_bg_color 444444
 tide_go_color 00ACD7
