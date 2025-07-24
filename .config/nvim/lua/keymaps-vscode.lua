974b1f0e52f78681006c377e53f1f0732a7b1ad2
reagenzn <aiooksa929@gmail.com>
2025-07-24 13:04:06 +0900

    oil-code追加

--- .config/nvim/lua/keymaps-vscode.lua
+++ .config/nvim/lua/keymaps-vscode.lua
@@ -30,3 +30,25 @@ keymap('n', ']', "<Cmd>lua require('vscode-neovim').call('editor.action.goToType
 
 -- ESC*2 でハイライトやめる
 keymap("n", "<Esc><Esc>", ":<C-u>set nohlsearch<Return>", opts)
+
+-- Default oil.code keymaps
+local vscode = require('vscode')
+local map = vim.keymap.set
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+    pattern = { "*" },
+    callback = function()
+        map("n", "-", function() vscode.action('oil-code.open') end)
+    end,
+})
+
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+    pattern = { "oil" },
+    callback = function()
+        map("n", "-", function() vscode.action('oil-code.openParent') end)
+        map("n", "_", function() vscode.action('oil-code.openCwd') end)
+        map("n", "<CR>", function() vscode.action('oil-code.select') end)
+        map("n", "<C-t>", function() vscode.action('oil-code.selectTab') end)
+        map("n", "<C-l>", function() vscode.action('oil-code.refresh') end)
+        map("n", "`", function() vscode.action('oil-code.cd') end)
+    end,
+})
